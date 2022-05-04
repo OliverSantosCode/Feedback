f39793887cce610606dc164db1a3f7a4274a829b
OliverSantosCode <oliversantoscode@gmail.com>
2022-05-04 10:55:04 -0300

    Feedback: server - Prisma: Send Email: Nodemailer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,46 @@
 import express from 'express';
+import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-   res.send('Hello World!\n');
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "3a8468924ca084",
+      pass: "e50fd791b2cd10"
+    }
+});
+
+app.post('/feedbacks', async (req, res) => {
+//    console.log(req.body);
+const { type, comment, screenshot } = req.body;
+ 
+const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    });
+
+await   transport.sendMail({
+        from: 'Equipe Feedback <oi@feedback.com>',
+        to: 'Oliver Santos <oliverdraftspr@gmail.com>',
+        subject: 'Novo Feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+
+    });
+
+    return res.status(201).json({data: feedback});
 })
 
 app.listen(3333, () => {
